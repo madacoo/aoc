8b7ed0fccd8a2bc71acc5dd061125bf1be0be5cf
madacoo <matthew@madacoo.uk>
2018-12-23 08:08:14 +0800

    2018, day 7, part two

--- 2018/07/solve.py
+++ 2018/07/solve.py
@@ -1,5 +1,23 @@
 from collections import defaultdict
 
+class Worker:
+    def __init__(self):
+        self.step = None
+        self.seconds = -1
+
+    def start_work(self, step):
+        if not step: return
+        self.step = step
+        self.seconds = seconds(step)
+
+    def update(self):
+        self.seconds -= 1
+        if self.seconds == 0:
+            step = self.step
+            self.step = None
+            return step
+        return ''
+
 
 def read_input():
     edges = defaultdict(list)
@@ -18,18 +36,29 @@ def has_dependency(node, edges, done):
 
 
 def next_step(candidates, edges, order):
-    return sorted([n for n in candidates
-                   if not has_dependency(n, edges, order)])[0]
+    try:
+        return sorted([n for n in candidates
+                       if not has_dependency(n, edges, order)])[0]
+    except IndexError:
+        return False
+
+
+def seconds(steps): return ord(steps)-4
     
 
-def solve1(edges):
-    order = ''
-
+def steps(edges):
     candidates = set()
     for parent, children in edges.items():
         candidates.add(parent)
         for child in children: candidates.add(child)
+    return candidates
 
+
+
+def solve1(edges):
+    order = ''
+
+    candidates = steps(edges)
     while candidates:
         order += next_step(candidates, edges, order)
         candidates.remove(order[-1])
@@ -37,8 +66,24 @@ def solve1(edges):
     return order
 
 
+def solve2(edges):
+    second = -1
+    done = ''
+    workers = [Worker() for _ in range(5)]
+
+    candidates = steps(edges)
+    while candidates or [w for w in workers if w.step]:
+        for w in workers:
+            done += w.update()
+            if not w.step:
+                w.start_work(next_step(candidates, edges, done))
+                if w.step: candidates.remove(w.step)
+        second += 1
+    return second
+
 
 if __name__ == '__main__':
     edges = read_input()
     print(solve1(edges))
+    print(solve2(edges))
 
